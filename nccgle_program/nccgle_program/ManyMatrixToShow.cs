53c65c7770376ac2da97a4deb8a0a0822bc1c9a9
tristan.ncc <tristan.ncc@e7c5392e-0993-11de-b55b-d397e7115e68>
2009-03-21 20:25:57 +0000

    Вова:     то, что в субботу забадяжили

---
+++ nccgle_program/nccgle_program/ManyMatrixToShow.cs
@@ -0,0 +1,63 @@
+﻿namespace nccgle_program
+{
+    /// <summary>
+    /// Структура для передачи данных 
+    /// для упрощения последующего отображения
+    /// </summary>
+    public struct ManyMatrixToShow
+    {
+
+        public Matrix KoeficientUniqI;
+        public Matrix KoeficientSvyaziI;
+
+        public Matrix KolInKlaster;
+        public Matrix SrInKlaster;
+
+        /// <summary>
+        ///  Собирательная способность i-го документа
+        /// </summary>
+        public Matrix Pi;
+
+        /// <summary>
+        /// Кластеры
+        /// </summary>
+        public Matrix Clusters;
+
+        /// <summary>
+        /// Дэлтьта
+        /// </summary>
+        public Matrix Delta;
+
+        /// <summary>
+        /// Дэльта S
+        /// </summary>
+        public Matrix DeltaS;
+
+        /// <summary>
+        /// Общий коэфициент уникальности 
+        /// </summary>
+        public double ObschKoeficientUnic;
+
+        /// <summary>
+        /// Общий коэфициент связи
+        /// </summary>
+        public double ObschKoeficientSvyazi;
+
+        /// <summary>
+        /// Вектор G
+        /// </summary>  
+        public Matrix OneAndZero;
+
+        /// <summary>
+        /// Число терминов
+        /// теоретическое
+        /// </summary>
+        public double TerminsNumber;
+
+        /// <summary>
+        /// Число кластеров
+        /// теоретическое
+        /// </summary>
+        public double ClustersNumber;
+    }
+}
