5fcfacb3b9aff314227c0fe48b5387a1cd61234a
nakedsnake.boschuk@gmail.com <nakedsnake.boschuk@gmail.com@e7c5392e-0993-11de-b55b-d397e7115e68>
2009-06-06 02:13:20 +0000

    МММ

--- nccgle_program/RExpertone/Form1.Designer.cs
+++ nccgle_program/RExpertone/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace RExpertone
+﻿using System.Collections.Generic;
+namespace RExpertone
 {
     partial class Form1
     {
@@ -33,14 +34,16 @@
             this.textBoxFormula = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxPeremen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonSet = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(353, 24);
+            this.button1.Location = new System.Drawing.Point(336, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -50,23 +53,22 @@
             // 
             // textBoxAns
             // 
-            this.textBoxAns.Location = new System.Drawing.Point(125, 26);
+            this.textBoxAns.Location = new System.Drawing.Point(127, 21);
             this.textBoxAns.Name = "textBoxAns";
             this.textBoxAns.Size = new System.Drawing.Size(33, 20);
             this.textBoxAns.TabIndex = 1;
             // 
             // textBoxFormula
             // 
-            this.textBoxFormula.Location = new System.Drawing.Point(183, 26);
+            this.textBoxFormula.Location = new System.Drawing.Point(185, 21);
             this.textBoxFormula.Name = "textBoxFormula";
             this.textBoxFormula.Size = new System.Drawing.Size(145, 20);
             this.textBoxFormula.TabIndex = 1;
-            this.textBoxFormula.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Location = new System.Drawing.Point(14, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 13);
             this.label1.TabIndex = 2;
@@ -75,24 +77,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(164, 29);
+            this.label2.Location = new System.Drawing.Point(166, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "=";
             // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(15, 193);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(413, 239);
-            this.webBrowser1.TabIndex = 4;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 59);
+            this.label3.Location = new System.Drawing.Point(17, 54);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(95, 13);
             this.label3.TabIndex = 5;
@@ -100,20 +94,46 @@
             // 
             // textBoxNumber
             // 
-            this.textBoxNumber.Location = new System.Drawing.Point(125, 59);
+            this.textBoxNumber.Location = new System.Drawing.Point(127, 54);
             this.textBoxNumber.Name = "textBoxNumber";
             this.textBoxNumber.Size = new System.Drawing.Size(33, 20);
             this.textBoxNumber.TabIndex = 6;
-            this.textBoxNumber.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBoxPeremen
+            // 
+            this.textBoxPeremen.Location = new System.Drawing.Point(185, 54);
+            this.textBoxPeremen.Name = "textBoxPeremen";
+            this.textBoxPeremen.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPeremen.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Доверительные итервалы:";
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.Location = new System.Drawing.Point(355, 97);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(35, 247);
+            this.buttonSet.TabIndex = 9;
+            this.buttonSet.Text = "set";
+            this.buttonSet.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 463);
+            this.ClientSize = new System.Drawing.Size(469, 590);
+            this.Controls.Add(this.buttonSet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPeremen);
             this.Controls.Add(this.textBoxNumber);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxFormula);
@@ -133,9 +153,13 @@
         private System.Windows.Forms.TextBox textBoxFormula;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxPeremen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonSet;
+
+
     }
 }
 
