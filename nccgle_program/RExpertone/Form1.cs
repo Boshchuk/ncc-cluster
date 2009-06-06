ec588076e6168d3f7de874d451a13e163ed3da9a
nakedsnake.boschuk@gmail.com <nakedsnake.boschuk@gmail.com@e7c5392e-0993-11de-b55b-d397e7115e68>
2009-06-06 02:13:20 +0000

    МММ

--- nccgle_program/RExpertone/Form1.cs
+++ nccgle_program/RExpertone/Form1.cs
@@ -12,9 +12,16 @@ namespace RExpertone
 {
     public partial class Form1 : Form
     {
+        Control ControlObject;
+        List<System.Windows.Forms.TextBox> DynTextBoxesL = new List<TextBox>();
+        List<System.Windows.Forms.TextBox> DynTextBoxesR = new List<TextBox>();
+        List<Matrix> PublicInterval = new List<Matrix>();
+
+
+        int peremencount;
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();           
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,21 +29,72 @@ namespace RExpertone
             Cooficient answer = new Cooficient(textBoxAns.Text);
 
             int n = int.Parse(textBoxNumber.Text);
+
+
             Matrix test = new Matrix(2,n);
 
-            //RExpertoneService.Rfrequency
+
+            peremencount = int.Parse( textBoxPeremen.Text);
+            Matrix ObobIntervals = new Matrix(2,peremencount);
+
+            makeTextBox(peremencount);
+          
+            
+            for (int i = 0; i < peremencount; i++)
+            {
+                Matrix M = new Matrix(1, 4);
+                PublicInterval.Add(M);
+            }
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        /// <summary>
+        /// добавляет пару текстбоксов , что бы ввести обобщенные интервалы
+        /// </summary>
+        private void makeTextBox(int parnumber)
         {
+            for (int i = 0; i < parnumber; i++)
+            {
+                TextBox DynTextBox;
 
+                System.Reflection.Assembly asm;
+                asm = typeof(Form).Assembly;
+                ControlObject = (System.Windows.Forms.Control)asm.CreateInstance(" System.Windows.Forms.TextBox");
+                ControlObject.Name = "texboxL"+i.ToString();
+                ControlObject.Location = new System.Drawing.Point(30, 130+(i*30));
+                this.Controls.Add(ControlObject);
+                
+
+                DynTextBox = (System.Windows.Forms.TextBox)ControlObject;
+                DynTextBox.Width = 50;
+                DynTextBox.Text = "Левая граница";
+                DynTextBox.Click += new System.EventHandler(DynCtrl_Event);
+                DynTextBoxesL.Add(DynTextBox);
+                
+            }
+            for (int i = 0; i < parnumber; i++)
+            {
+                TextBox DynTextBox;
+
+                System.Reflection.Assembly asm;
+                asm = typeof(Form).Assembly;
+                ControlObject = (System.Windows.Forms.Control)asm.CreateInstance(" System.Windows.Forms.TextBox");
+                ControlObject.Name = "texboxR" + i.ToString();
+                ControlObject.Location = new System.Drawing.Point(100, 130 + (i * 30));
+                this.Controls.Add(ControlObject);
+
+
+                DynTextBox = (System.Windows.Forms.TextBox)ControlObject;
+                DynTextBox.Width = 50;
+                DynTextBox.Text = "Правая граница";
+
+                DynTextBox.Click += new System.EventHandler(DynCtrl_Event);
+                DynTextBoxesL.Add(DynTextBox);                
+            }           
         }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void DynCtrl_Event(object sender, System.EventArgs e)
         {
-
-        }
-
-        
+            TextBox t = (TextBox)sender;
+            t.Text = "";
+        }        
     }
 }
